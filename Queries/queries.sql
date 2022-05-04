64d1cc5bc1ed57906cc2cbb6d80f4b0138c26536
fathi129 <nasreen.fathi@gmail.com>
2022-05-04 00:22:29 -0500

    Database created,imported csv files

---
+++ Queries/queries.sql
@@ -0,0 +1,28 @@
+select count(*) from departments;
+select count(*) from employees;
+select count(*) as dept_emp from dept_emp;
+
+select count(*) as dept_manager from dept_manager;
+
+select count(*) as salaries from salaries;
+
+select count(*) as titles from titles;
+select * from retirement_info;
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+into retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Retirement eligibility
+SELECT count(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
