f8539e9cf654990138c9abf3b97b770b5dbbbc6f
fathi129 <nasreen.fathi@gmail.com>
2022-05-15 20:31:54 -0500

    Additional Queries

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -7,7 +7,7 @@ select e.emp_no,
 		t.title,
 		t.from_date,
 		t.to_date		
-into retirement_titles
+--into retirement_titles
 from employees e 
 inner join titles t on e.emp_no = t.emp_no
 where (e.birth_date between '1952-01-01' and '1955-12-31')
@@ -21,7 +21,7 @@ select distinct on (r.emp_no) r.emp_no,
 		r.first_name,
 		r.last_name,
 		r.title
-into unique_titles
+--into unique_titles
 from retirement_titles r
 where r.to_date = '9999-01-01'
 order by r.emp_no asc,r.to_date desc;
@@ -51,6 +51,37 @@ where (de.to_date = '9999-01-01') and
 (e.birth_date between '1965-01-01' and '1965-12-31') 
 order by e.emp_no,t.from_date desc;
 
+--Additional Query:
+--To find the departments of the retiring employees
+SELECT DISTINCT ON (rt.emp_no) 
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title,
+	d.dept_name
+INTO unique_titles_department
+FROM retirement_titles as rt
+INNER JOIN dept_emp as de
+ON (rt.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (d.dept_no = de.dept_no)
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- How many roles will need to be fill per title and department?
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+--INTO roles_needed
+FROM (SELECT title, dept_name from unique_titles_department) as ut
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.title DESC;
+
+
+-- Qualified staff, retirement-ready employees to mentor next generation.
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO qualified_staff
+FROM (SELECT title, dept_name from unique_titles_department) as ut
+WHERE ut.title IN ('Senior Engineer', 'Senior Staff', 'Technique Leader', 'Manager')
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
 
 
 
